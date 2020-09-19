ea989adb41c841514470571c284b5456d13b9ed0
marong <marong@microsoft.com>
2020-09-19 13:40:29 +0800

    test

--- Deploy-AzureResourceGroup.ps1
+++ Deploy-AzureResourceGroup.ps1
@@ -21,7 +21,7 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 3
 
 function Format-ValidationOutput {
-    param ($ValidationOutput, [int] $Depth = 0)
+    param ($ValidationOutput, [int] $Depth = 0)fsgsgg
     Set-StrictMode -Off
     return @($ValidationOutput | Where-Object { $_ -ne $null } | ForEach-Object { @('  ' * $Depth + ': ' + $_.Message) + @(Format-ValidationOutput @($_.Details) ($Depth + 1)) })
 }
