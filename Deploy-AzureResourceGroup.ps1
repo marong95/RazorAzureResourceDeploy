b2af05047a4d648b945feec3ec1213c9051b69fa
marong95 <71007879+marong95@users.noreply.github.com>
2020-09-19 13:54:23 +0800

    Update Deploy-AzureResourceGroup.ps1

--- Deploy-AzureResourceGroup.ps1
+++ Deploy-AzureResourceGroup.ps1
@@ -22,7 +22,7 @@ Set-StrictMode -Version 3
 
 function Format-ValidationOutput {
     param ($ValidationOutput, [int] $Depth = 0)ttaaaattt
-    fddfadddafdd
+    
     Set-StrictMode -Off
     return @($ValidationOutput | Where-Object { $_ -ne $null } | ForEach-Object { @('  ' * $Depth + ': ' + $_.Message) + @(Format-ValidationOutput @($_.Details) ($Depth + 1)) })
 }
